df95f74cf7e6ddcb1de98ebe33cc51815e83eeb7
BuildEnough <rhtmaehcl878@naver.com>
2022-07-11 22:40:17 +0900

    정보처리기사 정리_20220711

--- codeup문제/codeup_6015.py
+++ codeup문제/codeup_6015.py
@@ -1,8 +1,19 @@
-from posixpath import split
+numbers = input().split()
+a = int(numbers[0])
+b = int(numbers[1])
+print(a)
+print(b)
 
 
+# 결과 동시 할당
 a, b = input().split()
 a = int(a)
 b = int(b)
 print(a)
 print(b)
+
+
+# 고급 방법
+a, b = map(int, input().split())
+print(a, type(a))
+print(b, type(b))
