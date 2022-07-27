b3cf5601e0ffbac76cbefd4e6444eebf91b7c465
BuildEnough <rhtmaehcl878@naver.com>
2022-07-27 13:00:07 +0900

    20220727 중간커밋

--- 노션파이썬문제/20220725_실습/2_두개뽑아서더하기.py
+++ 노션파이썬문제/20220725_실습/2_두개뽑아서더하기.py
@@ -15,3 +15,23 @@ def solution(numbers):
 
 print(solution([2, 1, 3, 4, 1]))
 print(solution([5, 0, 2, 7]))
+
+
+# def solution(numbers):
+#     answer = []
+    
+#     set_ = set()
+#     for i in range(len(numbers)): 
+#         for j in range(i+1,len(numbers)): 
+#             n1 = number[i]
+#             n2 = number[j]
+            
+#             sum_ = n1 + n2
+            
+#             set_.add(sum_)
+            
+#     # 순서가 없는 set를 순서가 없는 list로 형변환        
+#     list_ - list(set_)
+#     answer = sorted(list_)
+            
+#     return answer
