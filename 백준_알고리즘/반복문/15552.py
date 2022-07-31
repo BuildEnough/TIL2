3adc02373e7f098de4eae8a80d5c29d111697348
BuildEnough <rhtmaehcl878@naver.com>
2022-08-01 01:00:44 +0900

    20200731

---
+++ 백준_알고리즘/반복문/15552.py
@@ -0,0 +1,5 @@
+Testcase = int(input())
+
+for i in range(1, Testcase + 1):
+    F, S = map(int, input().split())
+    print(F + S)
---
+++ 백준_알고리즘/반복문/2739.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+i = 1
+
+while True:
+    print(f'{N} * {i} = {N * i}')
+    i += 1
+    if i >= 10:
+        break
