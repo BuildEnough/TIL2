2851ce308f7be341a50815eb0d6614ec8b7bbc01
김병우 <gimbyeong-u@gimbyeong-uui-MacBookPro.local>
2022-08-06 08:48:02 +0900

    백준 문제

--- 백준_알고리즘/1차원배열/8958.py
+++ 백준_알고리즘/1차원배열/8958.py
@@ -1,2 +1,17 @@
 import sys
 sys.stdin = open('8958.txt')
+
+count = 0
+result = 0
+
+for _ in range(int(input())):
+    quiz = list(input())
+    for i in quiz:
+        if i == 'O':
+            count += 1
+            result += count
+        elif i == 'X':
+            count = 0
+    print(result)
+    result = 0
+    count = 0
