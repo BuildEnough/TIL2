b45516f407fcb1b9d4140c960da0beb431fa26a6
BuildEnough <rhtmaehcl878@naver.com>
2022-08-06 10:13:55 +0900

    백준 1문제 푸는데 3시간 걸림(윈도우 PC)

--- 백준_알고리즘/1차원배열/4344.py
+++ 백준_알고리즘/1차원배열/4344.py
@@ -3,8 +3,14 @@ sys.stdin = open('4344.txt')
 
 for _ in range(int(input())):
     N = list(map(int, input().split()))
+    avg_ = sum(N[1:]) / N[0]
+    c = 0
+    for z in N[1:]:
+        if z > avg_:
+            c += 1
+    result = c / N[0] * 100
+    print(f'{result:.3f}%')
 
-    list_ = []
-    for i in range(1, len(N)):
-        print(N(i))
-    
+n = int(input())
+
+# >=인지 >인지 주의 여기서 못찾아서 1시간 더 걸림
