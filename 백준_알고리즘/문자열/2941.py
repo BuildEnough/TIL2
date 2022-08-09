28cfafa3899b17e45546c5eeb05415309aad93db
BuildEnough <rhtmaehcl878@naver.com>
2022-08-09 20:56:08 +0900

    20220809 백준 문제(윈도우PC)

--- 백준_알고리즘/문자열/2941.py
+++ 백준_알고리즘/문자열/2941.py
@@ -1,2 +1,20 @@
 import sys
 sys.stdin = open('2941.txt')
+
+word = input()
+
+c_list = [
+    'c=',
+    'c-',
+    'dz=',
+    'd-',
+    'lj',
+    'nj',
+    's=',
+    'z='
+]
+
+for i in c_list:
+    word = word.replace(i, '*')
+# print(word)
+print(len(word))
