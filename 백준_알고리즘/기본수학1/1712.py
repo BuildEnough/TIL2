665211496d22489984e44ac7d86b0aed00bfcd5a
BuildEnough <rhtmaehcl878@naver.com>
2022-08-10 21:13:11 +0900

    20220810 백준 문제 풀이(윈도우 PC)

--- 백준_알고리즘/기본수학1/1712.py
+++ 백준_알고리즘/기본수학1/1712.py
@@ -3,4 +3,14 @@ sys.stdin = open('1712.txt')
 
 A, B, C = map(int, input().split())
 
-if 
+# C * n = A + B * n
+# n = A / (C - B) 
+# A가 아무리 커도 B > C: 손익분기점 X
+# C > B: 손익분기점 O
+if B - C >= 0:
+    print(-1)
+else:
+    print(A // (C - B) + 1)
+# / 이걸하면 실수로 나옴 //로 해야함
+# 그리고 +1 해줘야함: 손익분기점인 때가 최초로 이익이 발생하기 때문
+ 
---
+++ 백준_알고리즘/기본수학1/2292.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('2292.txt')
+
+num = int(input())
+
+bee_house = 1
+count = 1
+
+while num > bee_house:
+    bee_house += 
