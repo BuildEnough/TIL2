89cc2976d68efec465b7c72da6496f6ac76feee7
BuildEnough <rhtmaehcl878@naver.com>
2022-08-11 06:52:01 +0900

    20220811 백준 문제(맥 OS)

--- 백준_알고리즘/기본수학1/2292.py
+++ 백준_알고리즘/기본수학1/2292.py
@@ -1,10 +1,19 @@
+from re import A
 import sys
 sys.stdin = open('2292.txt')
+
+# 1 -> 7 -> 19 -> 37
+# 6 -- 12 -- 18
 
 num = int(input())
 
 bee_house = 1
+a = 0
 count = 1
 
+
 while num > bee_house:
-    bee_house += 
+    a += 6
+    bee_house += a
+    count +=1
+print(count)
