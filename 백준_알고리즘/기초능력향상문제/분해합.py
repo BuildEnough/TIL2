5ff51c33182be2aa70f820ef1edec1a6debbd7cd
BuildEnough <rhtmaehcl878@naver.com>
2022-08-23 09:00:33 +0900

    백준 문제 풀이

--- 백준_알고리즘/기초능력향상문제/분해합.py
+++ 백준_알고리즘/기초능력향상문제/분해합.py
@@ -6,5 +6,13 @@ sys.stdin = open('분해합.txt')
 N = int(input())
 
 for i in range(1, N):
-    i = list(str(i))
-    print(i)
+    A = sum(map(int, str(i)))
+
+    result = i + A
+
+    if result == N:
+        print(i)
+        break
+else:
+    print(0)
+    
---
+++ 백준_알고리즘/기초능력향상문제/숫자의_개수.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2577
+
+import sys
+sys.stdin = open('숫자의_개수.txt')
+
+A = int(input())
+B = int(input())
+C = int(input())
+
+result = A * B * C
+
+list_ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
