f99d6112f42d4e08f15733c116e16673b5bb542f
BuildEnough <rhtmaehcl878@naver.com>
2022-08-24 17:51:29 +0900

    20220824 수업 종료

--- 백준_알고리즘/기초능력향상문제/새로운_불면증_치료법.py
+++ 백준_알고리즘/기초능력향상문제/새로운_불면증_치료법.py
@@ -1,2 +1,23 @@
+from re import A
 import sys
 sys.stdin = open('새로운_불면증_치료법.txt')
+
+for T in range(int(input())):
+    N = int(input())
+    
+    list_ = [0]*10
+
+    a = 0
+    while True:
+        try:
+            a += 1
+            i = str(N * a)
+            for j in i:
+                
+
+        except:
+            if list_ not in 0:
+                break
+
+
+        
