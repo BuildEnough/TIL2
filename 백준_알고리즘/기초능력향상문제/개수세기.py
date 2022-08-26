6c98f02a880cd88182b5818c339844bcec5d8786
BuildEnough <rhtmaehcl878@naver.com>
2022-08-26 10:05:04 +0900

    수업 전 문제풀이

--- 백준_알고리즘/기초능력향상문제/개수세기.py
+++ 백준_알고리즘/기초능력향상문제/개수세기.py
@@ -2,3 +2,18 @@
 
 import sys
 sys.stdin = open('개수세기.txt')
+
+N = int(input())
+
+n = list(input().split())
+
+v = input()
+
+count = 0
+for i in n:
+    if v == i: # if v in i:로 했는데 오류남 처음엔 개빡쳤는데 하다보니까 이유를 암
+                # in을 사용하면 있다는 것만 알고 i값이 2개라고 해도 되는데?
+                # 반복문 이니까 돌아가면서 v가 있기만 하면 되잖아
+                # 근데 왜 in은 답으로 안쳐주는지 개빡치네,,
+        count += 1
+print(count)
---
+++ 백준_알고리즘/기초능력향상문제/단어공부.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1157
+
+import sys
+sys.stdin = open('단어공부.txt')
+
+word = input()
+
+word = word.upper()
+
+print(word)
