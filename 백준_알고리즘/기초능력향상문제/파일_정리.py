d29966d8b65497e7c5f8f1b7ba1d7c3c775388a0
BuildEnough <rhtmaehcl878@naver.com>
2022-09-02 23:24:50 +0900

    백준 문제

--- 백준_알고리즘/기초능력향상문제/파일_정리.py
+++ 백준_알고리즘/기초능력향상문제/파일_정리.py
@@ -2,3 +2,19 @@
 
 import sys
 sys.stdin = open('파일_정리.txt')
+
+N = int(input())
+ext = {}
+for i in range(N):
+    f_n = input().strip()
+    file_name = f_n.split('.')[1]
+    if file_name not in ext:
+        ext[file_name] = 1
+    else:
+        ext[file_name] += 1
+
+file_name = list(ext.keys())
+
+file_name.sort()
+for z in file_name:
+    print(z, ext[z])
