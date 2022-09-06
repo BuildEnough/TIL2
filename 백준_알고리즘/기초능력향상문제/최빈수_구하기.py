70c0318fc5d63d1a7d8e64d62b719129eea1dcbb
BuildEnough <rhtmaehcl878@naver.com>
2022-09-07 01:47:42 +0900

    백준 문제 풀이

---
+++ 백준_알고리즘/기초능력향상문제/최빈수_구하기.py
@@ -0,0 +1,4 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV13zo1KAAACFAYh
+
+import sys
+sys.stdin = open('최빈수_구하기.txt')
--- 백준_알고리즘/기초능력향상문제/파일_정리.py
+++ 백준_알고리즘/기초능력향상문제/파일_정리.py
@@ -3,10 +3,30 @@
 import sys
 sys.stdin = open('파일_정리.txt')
 
+# for i in range(int(input())):
+#     file_ = input()
+#     for i in file_:
+#         if i == '.':
+#             print(file_.index('.'))
+
+
+file_dic = {}
 for i in range(int(input())):
-    file_ = input()
-    file_name = file_.lstrip('.')[1]
-    print(file_name)
+    file_name = input()
+    file_name = file_name.split('.')[1]
+    if file_name in file_dic:
+        file_dic[file_name] += 1
+    else:
+        file_dic[file_name] = 1
+
+file_name = list(file_dic.keys())
+
+file_name.sort()
+
+for j in file_name:
+    print(j, file_dic[j])
+
+
 
 
 
