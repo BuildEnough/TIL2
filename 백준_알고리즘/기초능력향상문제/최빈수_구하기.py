532d70972ed21236779146781664f2970cf3534e
BuildEnough <rhtmaehcl878@naver.com>
2022-09-10 22:11:14 +0900

    백준

--- 백준_알고리즘/기초능력향상문제/최빈수_구하기.py
+++ 백준_알고리즘/기초능력향상문제/최빈수_구하기.py
@@ -4,6 +4,27 @@ import sys
 sys.stdin = open('최빈수_구하기.txt')
 
 
+dic = {}
+for i in range(int(input())):
+    test = int(input())
+    list_ = input().split()
+    for i in list_:
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+            
+    dic_list = list(dic.values())
+    dic_list.sort()
+    
+    max_num = dic_list[-1]
+
+    for j in dic:
+        if str(dic[j]) in str(max_num):
+            print(j)
+
+
+
 
 T = int(input())
 for i in range(T):
