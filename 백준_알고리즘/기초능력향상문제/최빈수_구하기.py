a19f88cd3a491c64922b0af5fe4f71383723b13a
BuildEnough <rhtmaehcl878@naver.com>
2022-09-11 12:43:08 +0900

    백준 문제

--- 백준_알고리즘/기초능력향상문제/최빈수_구하기.py
+++ 백준_알고리즘/기초능력향상문제/최빈수_구하기.py
@@ -4,39 +4,19 @@ import sys
 sys.stdin = open('최빈수_구하기.txt')
 
 
-dic = {}
+
 for i in range(int(input())):
     test = int(input())
     list_ = input().split()
-    for i in list_:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
-            
-    dic_list = list(dic.values())
-    dic_list.sort()
-    
-    max_num = dic_list[-1]
-
-    for j in dic:
-        if str(dic[j]) in str(max_num):
-            print(j)
-
-
-
-
-T = int(input())
-for i in range(T):
-    idx = int(input())
-    arr = list(map(int, input().split()))
     score = dict()
-    for s in arr:
-        if s in score:
-            score[s] += 1
+    for i in list_:
+        if i in score:
+            score[i] += 1
         else:
-            score[s] = 1
-    _max = max(score.values())
+            score[i] = 1
 
-    print('#', end='')
-    print(idx, next(key for key, value in score.items() if value == _max))
+    max_ = max(score.values())
+
+    for key, value in score.items():
+        if value == max_:
+            print(f'#{test} {key}')
