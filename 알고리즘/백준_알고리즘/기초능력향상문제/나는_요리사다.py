034598689fe12ba5cee158ca97d4b3834ae1bc50
BuildEnough <rhtmaehcl878@naver.com>
2022-09-17 01:29:48 +0900

    백준문제

--- 알고리즘/백준_알고리즘/기초능력향상문제/나는_요리사다.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/나는_요리사다.py
@@ -1,4 +1,9 @@
 # https://www.acmicpc.net/problem/2953
 
 import sys
-sys.stdin = open('나는_요리사다')
+sys.stdin = open('나는_요리사다.txt')
+
+score = [sum(map(int, input().split())) for _ in range(5)]
+
+
+print(score.index(max(score))+1, max(score))
