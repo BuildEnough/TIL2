48caadd96e26cdeb77850caf3414a41e3c89b569
BuildEnough <rhtmaehcl878@naver.com>
2022-09-18 15:03:32 +0900

    백준 문제(MAC OS)

--- 알고리즘/백준_알고리즘/기초능력향상문제/덩치.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/덩치.py
@@ -3,3 +3,15 @@
 import sys
 sys.stdin = open('덩치.txt')
 
+
+body = []
+for i in range(int(input())):
+    w, h = map(int, input().split())
+    body.append((w, h))
+
+for i in body:
+    a = 1
+    for j in body:
+        if i[0] < j[0] and i[1] < j[1]:
+            a += 1
+    print(a, end = ' ')
--- 알고리즘/백준_알고리즘/기초능력향상문제/직사각형_네개의_합집합의_면적_구하기.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/직사각형_네개의_합집합의_면적_구하기.py
@@ -2,3 +2,11 @@
 
 import sys
 sys.stdin = open('직사각형_네개의_합집합의_면적_구하기.txt')
+
+
+square = []
+for i in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    square.append((x1, y1, x2, y2))
+
+print(square)
