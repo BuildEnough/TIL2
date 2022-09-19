114400ce31cc0af215f8d7b05878431a9eac009c
BuildEnough <rhtmaehcl878@naver.com>
2022-09-20 00:27:28 +0900

    백준 문제(MAX OS)

--- 알고리즘/백준_알고리즘/기초능력향상문제/직사각형_네개의_합집합의_면적_구하기.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/직사각형_네개의_합집합의_면적_구하기.py
@@ -4,9 +4,15 @@ import sys
 sys.stdin = open('직사각형_네개의_합집합의_면적_구하기.txt')
 
 
-square = []
-for i in range(4):
+square = [[0] * 101 for _ in range(101)]
+for _ in range(4):
     x1, y1, x2, y2 = map(int, input().split())
-    square.append((x1, y1, x2, y2))
 
-print(square)
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            square[i][j] = 1
+
+result = 0
+for z in square:
+    result += sum(z)
+print(result)
