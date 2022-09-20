9c61e4b43756c98f7cef9fb86d5348645702d899
BuildEnough <rhtmaehcl878@naver.com>
2022-09-20 23:57:10 +0900

    백준 문제

--- 알고리즘/백준_알고리즘/기초능력향상문제/2차원_배열의_합.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/2차원_배열의_합.py
@@ -2,3 +2,13 @@
 
 import sys
 sys.stdin = open('2차원_배열의_합.txt')
+
+n, m = map(int, input().split())
+
+list_ = []
+
+for _ in range(n):
+    array = input().split()
+    list_.append(array)
+
+k = int(input())
