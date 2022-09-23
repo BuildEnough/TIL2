80e6038777404cdd13be3e25c6c7642d740f198f
BuildEnough <rhtmaehcl878@naver.com>
2022-09-24 01:25:00 +0900

    백준 문제

--- 알고리즘/백준_알고리즘/기초능력향상문제/문서_검색.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/문서_검색.py
@@ -2,3 +2,19 @@
 
 import sys
 sys.stdin = open('문서_검색.txt')
+
+document = input()
+word = input()
+
+result = 0
+index = 0
+
+for i in range(len(document)):
+    if index > i:
+        continue
+    if word == document[i: i + len(word)]:
+        result += 1
+        index = i + len(word)
+
+print(result)
+        
