55963a545c8103acf8249971a35dea3bbb98ace2
BuildEnough <rhtmaehcl878@naver.com>
2022-09-24 10:02:29 +0900

    DFS

--- 알고리즘/백준_알고리즘/기초능력향상문제/바이러스.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/바이러스.py
@@ -2,3 +2,16 @@
 
 import sys
 sys.stdin = open('바이러스.txt')
+
+computer = int(input())
+
+pair = int(input())
+
+arr = [[]*computer for _ in range(computer)]
+
+for _ in range(pair):
+    a, b = map(int, input().split())
+    arr[a].append[b]
+    arr[b].append[a]
+
+    
