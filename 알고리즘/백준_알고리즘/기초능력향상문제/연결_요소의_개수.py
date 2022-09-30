5d557c9cdc34079ae56cbec84c90c2cd4c10f884
BuildEnough <rhtmaehcl878@naver.com>
2022-10-01 00:16:08 +0900

    백준 문제풀이 중간

--- 알고리즘/백준_알고리즘/기초능력향상문제/연결_요소의_개수.py
+++ 알고리즘/백준_알고리즘/기초능력향상문제/연결_요소의_개수.py
@@ -1,4 +1,10 @@
 # https://www.acmicpc.net/problem/11724
 
 import sys
-sys.stdin = open('연결_요소의_개수.txt')
+
+sys.stdin = open("연결_요소의_개수.txt")
+
+n, m = map(int, input().split())
+
+# graph = [[] for _ in range(n + 1)]
+# visited = [False] * (n + 1)
